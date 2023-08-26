1c3a8811a0721aba8ed3cf99f172e9b63c83ea2c
Vladimir <vladimircuriel@outlook.com>
2023-08-26 17:36:34 -0400

    Added check to see if there was any error and Added saveData

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 // Initialization of the variables and DOM.
-
 const nickInput = document.getElementById("nick");
 const sizeInput = document.getElementById("size");
 const entryForm = document.getElementById("entryForm");
 const error = document.getElementById("error");
+
+console.log("🚀 ~ file: app.js:9 ~ sessionStorage.getItem:",sessionStorage.getItem("error"));
+// Check if there are errors in the sessionStorage.
+if (sessionStorage.getItem("error") != null)
+{
+   error.innerText = sessionStorage.getItem("error");
+   sessionStorage.removeItem("error");
+}
 
 // Event Listeners functions
 function checkForm(event) {
@@ -23,5 +30,14 @@ function checkForm(event) {
    }
    return true;
 }
+
+function saveData() {
+   setUserData(nickInput);
+   usersList(nickInput);
+}
+
 //Event Initialization
-entryForm.addEventListener("submit",checkForm);
+entryForm.addEventListener("submit",function (event) {
+   if (checkForm(event))
+      saveData();
+});
